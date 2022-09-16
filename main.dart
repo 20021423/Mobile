18027c9f3da89fae6ea74dbcc65c9ad4ee8546c3
damanhduc140902 <80797626+damanhduc140902@users.noreply.github.com>
2022-09-16 13:18:46 +0700

    HelloWorld-DamAnhDuc

--- main.dart
+++ main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const Center(
-    child: Text('Hello World!', textDirection: TextDirection.ltr),
+  runApp(const MaterialApp(
+    home: SafeArea (
+      child: Scaffold (
+        body: Center (child: Text('Hello World'),)     
+      ),
+    ),
   ));
 }
